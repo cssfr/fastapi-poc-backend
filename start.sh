557f27ac4ef47d592171a8a39fb9bdbca2d84ab4
Css FR <andrewrenegpt@gmail.com>
2025-06-25 20:22:15 +0200

    .

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo "DATABASE_URL is set: ${DATABASE_URL:+Yes}"
 echo "Running database migrations..."
 alembic upgrade head || { echo "Migration failed!"; exit 1; }
 echo "Migrations complete. Starting server..."
-# exec uvicorn app.main:app --host 0.0.0.0 --port 80 --forwarded-allow-ips "*" --proxy-headers
-exec uvicorn app.main:app --host 0.0.0.0 --port 80 
+exec uvicorn app.main:app --host 0.0.0.0 --port 80 --forwarded-allow-ips "*" --proxy-headers
+# exec uvicorn app.main:app --host 0.0.0.0 --port 80 
